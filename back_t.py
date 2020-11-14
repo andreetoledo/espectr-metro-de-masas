76a5907e6ddc8975696461c05314ce59cec72c95
Paula0020 <53914179+Paula0020@users.noreply.github.com>
2020-11-13 23:37:33 -0600

    Create back_t.py

---
+++ back_t.py
@@ -0,0 +1,15 @@
+"""
+María Paula Valdés
+Hector Aaron Pivaral
+Carné: 19146
+Carne: 19640
+"""
+class particula:
+    def __init__(self, name,mass,charge):
+        self.name = name
+        self.mass = mass
+        self.charge = charge
+        
+        
+    
+    
